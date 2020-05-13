e3b5c956baca3a37f6ea3a0001cbb2049f338d50
cuongnnc2610 <cuongnnc2610@gmail.com>
2020-05-13 18:06:08 +0700

    Lesson01 - CODE - 07.01.2020

---
+++ lesson01-setup-environment/src/main/java/cuong/lesson01_setup_environment/App.java
@@ -0,0 +1,13 @@
+package cuong.lesson01_setup_environment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
