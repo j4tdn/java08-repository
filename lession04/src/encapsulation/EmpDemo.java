8403e736bc59341361cd7e22993bc418ca280377
minhhoang2404 <au01657286171@gmail.com>
2020-05-15 22:43:48 +0700

    Lesson04 15.05.2020

---
+++ lession04/src/encapsulation/EmpDemo.java
@@ -0,0 +1,9 @@
+package encapsulation;
+
+public class EmpDemo {
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setYear(1998);
+		System.out.println(emp.getAge());
+	}
+}
---
+++ lession04/src/view/EncapDemo.java
@@ -0,0 +1,13 @@
+package view;
+
+import encapsulation.Employee;
+
+public class EncapDemo {
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setYear(1997);
+		// access modifier : default || protected
+		// System.out.println(emp.getAge());
+
+	}
+}
