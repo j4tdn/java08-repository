bade396695c8fe3dbe842008ef5fa32363d69ece
BaoYen <vothibaoyen10a1nh@gmail.com>
2020-06-02 20:51:21 +0700

    Lesson06-2/6/2020

---
+++ lesson06-exception/src/main/java/execution/Ex07.java
@@ -0,0 +1,54 @@
+package execution;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Ex07 {
+	private static FileWriter fw =null;
+	public static void main(String[] args)  {
+		File file = new File("data.txt");
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		//write data into file
+		try {
+			writeData(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}finally {
+			//save file
+			try {
+				fw.close();
+				// mo file tu dong 
+				Desktop.getDesktop().open(file);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			}			
+		}
+	}
+
+	private static void writeData(File file) throws IOException {
+			fw=new FileWriter(file);
+			fw.write("Java\t");
+			
+			// interrupt
+			fw.write(1/0);
+			fw.write("PHP\t");
+			fw.write("C#");
+			
+			
+			
+			System.out.println("Write file secessful !");
+		
+	}
+
+	
+}
