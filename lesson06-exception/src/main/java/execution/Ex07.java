32ba0266f4f4e54c29c8d9b8a3f7034b97a27ffb
DauThuy <dauthithuy99@gmail.com>
2020-06-02 20:52:30 +0700

    lesson06 02/06/2020

---
+++ lesson06-exception/src/main/java/execution/Ex07.java
@@ -0,0 +1,57 @@
+package execution;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Ex07 {
+	
+	private static FileWriter fw = null;
+	
+	public static void main(String[] args) {
+		File file = new File("data.txt");
+
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// write data into file
+		try {
+			writeData(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally { //luon thuc thi ctr du co exception
+			
+			try {
+				//save file
+				fw.close();
+				
+				//open file
+				Desktop.getDesktop().open(file);
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void writeData(File file) throws IOException {
+		fw = new FileWriter(file);
+		fw.write("JAVA\n");
+		
+		//interrupt: bi ngat
+		fw.write(1/0);
+		
+		fw.write("PHP\n");
+		fw.write("C#\n");
+		
+		
+		//System.out.println("Write file sucessful");
+	}
+}
