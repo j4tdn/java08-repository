51c56bffa23794ec2b670ec46883d8f71bbbe42f
hauviptt97 <hauviptt@gmail.com>
2020-06-23 22:45:04 +0700

    lesson07-libraries date 23.06.2020

---
+++ lesson07-libraries/src/main/java/format/Ex01.java
@@ -0,0 +1,19 @@
+package format;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		// locale, timezone, UTC GMT
+		System.out.println("def: " + Locale.getDefault());
+		// da_DK: demark
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("vi", "VN"));
+		System.out.println(nf.format(7100));
+
+		long number = 123456789;
+		DecimalFormat df = new DecimalFormat("#,###.000");
+		System.out.println("formatted: " + df.format(number));
+	}
+}
