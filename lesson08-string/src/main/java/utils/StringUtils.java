181aba7355cd9a509da44294dbbd082bf2e7dffb
cuongnnc2610 <cuongnnc2610@gmail.com>
2020-06-26 22:43:25 +0700

    lesson08 26.06.2020

---
+++ lesson08-string/src/main/java/utils/StringUtils.java
@@ -0,0 +1,27 @@
+package utils;
+
+import immutable.StringCounter;
+
+public class StringUtils {
+
+	private StringUtils() {
+	}
+
+	public static StringCounter count(String s) {
+		int numberOfLowerChars = 0;
+		int numberOfUpperChars = 0;
+		int numberOfDigits = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isDigit(s.charAt(i))) {
+				numberOfDigits++;
+			} else if (Character.isLowerCase(s.charAt(i))) {
+				numberOfLowerChars++;
+			} else if (Character.isUpperCase(s.charAt(i))) {
+				numberOfUpperChars++;
+			}
+		}
+
+		return new StringCounter(numberOfLowerChars, numberOfUpperChars, numberOfDigits);
+	}
+}
