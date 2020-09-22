5f4a033d1affa80c541a625e913ef37e54ff024a
cuongnnc2610 <cuongnn@vnext.com.vn>
2020-09-22 22:46:18 +0700

    lesson10 22.09.2020

---
+++ lesson10-arrays/src/main/java/declaration/Ex02.java
@@ -0,0 +1,51 @@
+package declaration;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		int[] digits = {1, 7, 2, 9, 11, 12};
+		int[] oddDigits = getOddDigits(digits);
+		printf(oddDigits);
+		
+		System.out.println("================================");
+		
+		// 2nd stream
+		int[] oddDigits2nd = Arrays.stream(digits)
+			.filter(digit -> digit % 2 != 0)
+			.toArray();
+		printf(oddDigits2nd);
+		
+		// cal sum
+		int sum = Arrays.stream(digits)
+				.filter(digit -> digit % 2 == 0)
+				.sum();
+		System.out.println("sum: " + sum);
+		
+		// find max
+		OptionalInt optionalMax = Arrays.stream(digits).max();
+		System.out.println("max: " + optionalMax.orElse(0));
+		
+	}
+	
+	public static int[] getOddDigits(int[] digits) {
+		int[] result = new int[digits.length];
+		int rindex = 0;
+		for (int digit : digits) {
+			if (digit % 2 != 0) {
+				result[rindex++] = digit;
+			}
+		}
+		return Arrays.copyOfRange(result, 0, rindex);
+	}
+	
+	private static void printf(int[] digits) {
+		for (int digit : digits) {
+			System.out.print(digit + " ");
+		}
+		System.out.println();
+	}
+
+}
