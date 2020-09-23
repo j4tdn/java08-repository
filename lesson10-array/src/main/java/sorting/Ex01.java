822104423608d2000b1b59c5a5e2483ac96b7516
ctdung27 <caotiendung2020@gmail.com>
2020-09-23 22:45:30 +0700

    Lesson10-22.09.2020

---
+++ lesson10-array/src/main/java/sorting/Ex01.java
@@ -0,0 +1,26 @@
+package sorting;
+
+import java.util.Arrays;
+
+//int[] , String[], Item[]
+public class Ex01 {
+	public static void main(String[] args) {
+		int[] numbers= {1,5,6,3,2,8};
+		
+		//sort(number);
+		Arrays.sort(numbers);
+		Arrays.stream(numbers).forEach(number -> System.out.println(number + " "));
+	}
+	
+	public static void sort(int[] numbers) {
+		for(int out = numbers.length-1;out>0;out--) {
+			for(int in = 0; in<out ; in++) {
+				if(numbers[in] > numbers[out]) {
+					int tmp = numbers[in];
+					numbers[in] = numbers[out];
+					numbers[out] = tmp;
+				}
+			}
+		}
+	}
+}
