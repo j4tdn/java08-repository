9f8ea99113ff42a35f165b7887535856979f3be3
= <tramtruongrcm@gmail.com>
2020-09-25 22:22:20 +0700

    lesson10 25/09/2020

--- lesson10-array/src/main/java/bean/Item.java
+++ lesson10-array/src/main/java/bean/Item.java
@@ -29,7 +29,11 @@ public class Item {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return id + " " + name;
+	}
+
 
 }
