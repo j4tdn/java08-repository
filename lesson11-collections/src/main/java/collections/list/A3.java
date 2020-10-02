2b08d7daa460f5af0a9f830dea96489a2c1e7098
ctdung27 <caotiendung2020@gmail.com>
2020-10-02 22:41:59 +0700

    Lesson11-02.10.2020

--- lesson11-collections/src/main/java/collections/list/A3.java
+++ lesson11-collections/src/main/java/collections/list/A3.java
@@ -1,6 +1,7 @@
 package collections.list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import bean.Item;
@@ -8,15 +9,12 @@ import bean.Item;
 public class A3 {
 	public static void main(String[] args) {
 	
-		
-		
-		
-		
-		
 		List<Item> items = getItems();
 		
 		//remove phải override lại hàm equals
-		items.remove(new Item(2,"B"));
+		//items.remove(new Item(2,"B"));
+		
+		//items.set(0, new Item(7,"X"));
 		
 		
 		
@@ -24,14 +22,26 @@ public class A3 {
 	}
 	
 	private static List<Item> getItems(){
-		List<Item> items = new ArrayList<>();
-		items.add(new Item(1,"A"));
-		items.add(new Item(2,"B"));
-		items.add(new Item(3,"C"));
-		items.add(new Item(4,"D"));
-		items.add(new Item(5,"E"));
-		items.add(new Item(6,"F"));
+//		List<Item> items = new ArrayList<>();
+//		items.add(new Item(1,"A"));
+//		items.add(new Item(2,"B"));
+//		items.add(new Item(3,"C"));
+//		items.add(new Item(4,"D"));
+//		items.add(new Item(5,"E"));
+//		items.add(new Item(6,"F"));
 		
-		return items;
+		
+//		List<Item> items = Arrays.asList(
+//				new Item(1,"A"),
+//				new Item(2,"B"),
+//				new Item(3,"C"),
+//				new Item(4,"D")
+				
+//				);
+		
+		//unmodifiable, immutable
+		return List.of(new Item(1,"A"),
+				new Item(1,"A"),
+				new Item(1,"A"));
 	}
 }
