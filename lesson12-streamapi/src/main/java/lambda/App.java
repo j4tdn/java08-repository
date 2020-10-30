8413e996d4ab7fd34d64042df55a893d8bead160
cuongnnc2610 <cuongnn@vnext.com.vn>
2020-10-30 22:43:01 +0700

    lesson 12 30.10.2020

---
+++ lesson12-streamapi/src/main/java/lambda/App.java
@@ -0,0 +1,11 @@
+package lambda;
+
+import java.util.function.BinaryOperator;
+
+public class App {
+
+	public static void main(String[] args) {
+		BinaryOperator<String> operator = (s1, s2) -> "";
+	}
+
+}
---
+++ lesson12-streamapi/src/main/java/lambda/ConstructorReference.java
@@ -0,0 +1,39 @@
+package lambda;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class ConstructorReference {
+
+	public static void main(String[] args) {
+		
+		Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5);
+		stream.forEach(System.out::println);
+		
+		Supplier<Item> noArgs = Item::new;
+		Item item1 = noArgs.get();
+		System.out.println("item1 id: " + item1.getId());
+		Function<Integer, Item> oneArg = Item::new;
+		Item item2 = oneArg.apply(100);
+		System.out.println("item2 id: " + item2.getId());
+	}
+
+}
+
+class Item {
+	private int id;
+
+	public Item() {
+		System.out.println("NoArgs");
+	}
+
+	public Item(int id) {
+		System.out.println("OneArgs");
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+}
