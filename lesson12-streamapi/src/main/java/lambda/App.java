144b6c35f995f139989449c67df26346b4fd068d
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-11-03 22:46:25 +0700

    Lesson12-streamapi-03/11/2020

--- lesson12-streamapi/src/main/java/lambda/App.java
+++ lesson12-streamapi/src/main/java/lambda/App.java
@@ -1,8 +1,13 @@
 package lambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class App {
 	public static void main(String[] args) {
@@ -15,5 +20,20 @@ public class App {
 		BinaryOperator<String> func = (s1, s2) -> s1;	
 		Comparator<String> comp = Comparator.comparing(Function.identity());
 		
+		List<String> items = Arrays.asList("x", "yy", "zz", "t");
+		items.stream().filter(t -> t.equals("x")).collect(Collectors.toList());
+		
+		// operations
+		System.out.println("================");
+		items.stream()
+		.filter(item -> {
+			System.out.println("1: " + item);
+			return item.contains("y");
+		}).map(item -> {
+			System.out.println("2: " + item);
+			return item.length();
+		})
+		.limit(1)
+		.collect(Collectors.toList());
 	}
 }
---
+++ lesson12-streamapi/src/main/java/view/Transactional.java
@@ -0,0 +1,78 @@
+package view;
+
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import bean.Trader;
+import bean.Transaction;
+import static java.util.stream.Collectors.*;
+
+import java.util.Arrays;
+
+import static java.util.Comparator.*;
+
+public class Transactional {
+	public static void main(String[] args) {
+		List<Trader> traders = getTrader();
+		List<Transaction> transactions = getTransaction(traders);
+		
+		// 1. Find all transactions in the year 2011 and sort them by value asc
+		List<Transaction> first = transactions.stream()
+					.filter(t -> t.getYear() == 2011)
+					.sorted(comparing(Transaction::getValue))
+					.collect(toList());
+		
+		// 2. Find all transactions have value greater than 300 and sort them by trader’s city
+		List<Transaction> second = transactions.stream()
+				.filter(t -> t.getValue() > 300)
+				.sorted(comparing(t -> t.getTrader().getCity()))
+				.collect(toList());
+		
+		// 3. What are all the unique cities where the traders work?
+		List<String> third = traders.stream().map(Trader::getCity).distinct().collect(toList());
+		
+		// 3.1 What are all the unique transactions. distinct by property
+		// C1
+		transactions.stream()
+				.collect(toMap(Transaction::getId, Function.identity(), (t1,t2) -> t1, LinkedHashMap::new));
+		
+		// C2
+		transactions.stream().filter(distinctByKey(Transaction::getId))
+							 .collect(toList()).forEach(System.out::println);
+		
+		// 3.2
+		List<Integer> numbers = Arrays.asList(2,1,2,1,3,3,4);
+		Map<Integer, Long> qtyMap = numbers.stream()
+				.collect(groupingBy(Function.identity(), counting()));
+	}
+	
+	// type reference
+	private static <T, R> Predicate<T> distinctByKey(Function<T, R> func) {
+		Set<R> uniqueTrans = new HashSet<>();
+		return t -> uniqueTrans.add(func.apply(t));
+	}
+	
+	public static List<Trader> getTrader() {
+		return List.of(
+				new Trader("Raoul", "Cambridge"),
+				new Trader("Mario", "Milan"),
+				new Trader("Alan", "Cambridge"),
+				new Trader("Brian", "Cambridge"));       
+	}
+	
+	public static List<Transaction> getTransaction(List<Trader> traders) {
+		return List.of( 
+		        new Transaction(1, traders.get(0), 2011, 300),
+		        new Transaction(2, traders.get(1), 2012, 1000),
+		        new Transaction(3, traders.get(0), 2011, 400),
+		        new Transaction(4, traders.get(1), 2012, 710),
+		        new Transaction(3, traders.get(3), 2012, 700),
+		        new Transaction(6, traders.get(2), 2012, 950));
+	}
+		
+}
