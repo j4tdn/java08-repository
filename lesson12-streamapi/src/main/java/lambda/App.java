a60a9bc592e8c1a2f2f38b8efe1b703c64f41b8b
ctdung27 <caotiendung2020@gmail.com>
2020-11-03 21:47:06 +0700

    Lesson12-03.11.2020

--- lesson12-streamapi/src/main/java/lambda/App.java
+++ lesson12-streamapi/src/main/java/lambda/App.java
@@ -1,8 +1,11 @@
 package lambda;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class App {
 	public static void main(String[] args) {
@@ -11,18 +14,31 @@ public class App {
 		//para
 		// ->
 		//{body}
-		Runnable runnable	= () -> {
-			System.out.println();
-		};
-		 
-		Function<String, Integer> function = (String s) -> {
-			return 1;
-		};
-		BinaryOperator<String> binaryOperator = ( s1, s2) -> "";
+//		Runnable runnable	= () -> {
+//			System.out.println();
+//		};
+//		 
+//		Function<String, Integer> function = (String s) -> {
+//			return 1;
+//		};
+//		BinaryOperator<String> binaryOperator = ( s1, s2) -> "";
+//		
+//		Comparator<String> comp = (s1,s2) -> {
+//			return s1.compareTo(s2);
+//		};
 		
-		Comparator<String> comp = (s1,s2) -> {
-			return s1.compareTo(s2);
-		};
+		List<String> items = Arrays.asList("x","yy","yz","z");
+		//operations
+		items.stream()
+		.filter(item -> {
+			System.out.println("1: "+item);
+			return item.contains("y");
+		})
+		.map(item -> {
+			System.out.println("2: "+ item);
+			return item.length();
+		})
+		.collect(Collectors.toList());
 	}
 
 }
