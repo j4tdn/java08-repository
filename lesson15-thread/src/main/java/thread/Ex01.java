aeaa0f75c8298a469de3bd939fb5859fa00da3a9
Tran Quan Lam <quanlamtran@gmail.com>
2021-01-26 22:45:53 +0700

    lesson15-thread 26/01/2021

---
+++ lesson15-thread/src/main/java/thread/Ex01.java
@@ -0,0 +1,27 @@
+package thread;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		System.out.println("Thread main start");
+		// Thread#start => check state => Thread#run => Runnable#run
+		Thread t0 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				System.out.println(Thread.currentThread().getName() + " is running...");
+			}
+		}, "t0");
+
+		t0.start();
+		int i = 0;
+		while (true) {
+			if (i == 10000000) {
+				System.out.println("Thread main end");
+				break;
+			}
+			i++;
+		}
+
+	}
+}
---
+++ lesson15-thread/src/main/java/thread/Ex03.java
@@ -0,0 +1,40 @@
+package thread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Ex03 {
+//	private static volatile int counter = 2;
+
+	private static AtomicInteger counter = new AtomicInteger(2);
+
+	public static void main(String[] args) {
+		System.out.println("Thread main start");
+		Thread t0 = new Thread(new Runnable() {
+			// expensive memory
+			// do not know execution task
+			// can't get return value from task
+			@Override
+			public void run() {
+				System.out.println("t0 is running ...");
+
+				System.out.println(counter.addAndGet(1) + " t0 counter ");
+			}
+		}, "t0");
+		t0.start();
+
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("t1 is running ...");
+				System.out.println(counter.addAndGet(4) + " t1 counter ");
+			}
+		}, "t0");
+
+		t1.start();
+
+		System.out.println("Thread main running...");
+		System.out.println(counter.addAndGet(2) + " main counter ");
+		System.out.println("Thread main end");
+	}
+}
