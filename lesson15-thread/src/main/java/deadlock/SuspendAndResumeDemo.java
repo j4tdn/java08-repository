d57f4b2620200ede5340b2c715d9b3835103dfb4
Cuong Nguyen <cuongnnc2610@gmail.com>
2021-03-02 23:10:52 +0700

    lesson18 02.03.2021

---
+++ lesson15-thread/src/main/java/deadlock/SuspendAndResumeDemo.java
@@ -0,0 +1,47 @@
+package deadlock;
+
+import java.util.concurrent.TimeUnit;
+
+public class SuspendAndResumeDemo {
+	
+	private static Object sharedData = new Object();
+	
+	public static void main(String[] args) throws Exception {
+		Thread thread1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Thread 1 has started...");
+				
+				synchronized (sharedData) {
+					System.out.println("Thread 1 has obtained lock on  >> sharedData <<");
+					
+					System.out.println("Thread 1 has suspended");
+					Thread.currentThread().suspend();
+					
+					System.out.println("Thread 1 has released >> sharedData <<");
+				}
+				System.out.println("Thread 1 has ended...");
+			}
+		});
+		thread1.start();
+		
+		// ensure thread1 starts by thread2
+		TimeUnit.SECONDS.sleep(2);
+
+		Thread thread2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Thread 2 has started...");
+				thread1.resume();
+				synchronized (sharedData) {
+					System.out.println("Thread 2 has obtained lock on  >> sharedData <<");
+					System.out.println("Thread 2 has released >> sharedData <<");
+				}
+				System.out.println("Thread 2 has ended...");
+			}
+		});
+		thread2.start();
+	}
+}
---
+++ lesson15-thread/src/main/java/quartz/BillJob.java
@@ -0,0 +1,15 @@
+package quartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class BillJob implements Job {
+
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		System.out.println("==== READY ====");
+		System.out.println("Querying data from database");
+		System.out.println("==== DONE ====");
+	}
+}
