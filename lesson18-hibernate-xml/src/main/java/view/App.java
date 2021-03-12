01b7680a8f425f172663dd87e2d9b9f4d9ded619
Cuong Nguyen <cuongnnc2610@gmail.com>
2021-03-12 22:22:00 +0700

    lesson18 12.03.2021

--- lesson18-hibernate-xml/src/main/java/view/App.java
+++ lesson18-hibernate-xml/src/main/java/view/App.java
@@ -1,12 +1,22 @@
 package view;
 
-import org.hibernate.Session;
+import java.util.List;
 
-import utils.HibernateUtil;
+import dao.HibernateStudentDao;
+import dao.StudentDao;
+import persistence.Student;
 
 public class App {
+	
+	private static StudentDao studentDao;
+	
+	static {
+		studentDao = new HibernateStudentDao();
+	}
+	
 	public static void main(String[] args) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		System.out.println("session: " + session);
+		studentDao.save(new Student(2, "Chau", "Ngoc", "ngocchau@gmail.com"));
+		List<Student> students = studentDao.getAll();
+		System.out.println("size: " + students.size());
 	}
 }
