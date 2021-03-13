6a90b3ac10eb8815bb961c4743090c253ffd613e
Cuong Nguyen <cuongnnc2610@gmail.com>
2021-03-13 17:31:34 +0700

    lesson18 13.03.2021

---
+++ lesson18-hibernate-config/src/main/java/dao/HibernateStudentDao.java
@@ -0,0 +1,75 @@
+package dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
+import org.hibernate.transform.Transformers;
+import org.hibernate.type.StandardBasicTypes;
+
+import persistence.GradeInfo;
+import persistence.Student;
+
+public class HibernateStudentDao extends AbstractHibernateDao implements StudentDao {
+
+	public void save(Student student) {
+		Session session = getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			session.saveOrUpdate(student);
+		} catch (Exception e) {
+			transaction.rollback();
+		}
+		transaction.commit();
+	}
+
+	public List<Student> getAll() {
+		Session session = getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+//		List<Student> students = session.createNamedQuery(Student.GET_ALL, Student.class).getResultList();
+		String query = "SELECT * FROM sinhvien";
+		List<Student> students = session.createNativeQuery(query, Student.class).getResultList();
+		transaction.commit();
+		return students;
+	}
+
+	public List<GradeInfo> getGradeInfos() {
+		Session session = getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		
+		String sql = "SELECT l.tenlop " + GradeInfo.GRADE_NAME + ",\n"
+				+ "	   concat(sv.ho, ' ', sv.ten) " + GradeInfo.STUDENT_NAME + ",\n"
+				+ "       sv.email " + GradeInfo.EMAIL + "\n"
+				+ "FROM sinhvien sv\n"
+				+ "JOIN lop l ON l.malop = sv.malop;";
+		
+		NativeQuery<?> query = session.createNativeQuery(sql);
+		
+		query.addScalar(GradeInfo.GRADE_NAME, StandardBasicTypes.STRING)
+			.addScalar(GradeInfo.STUDENT_NAME, StandardBasicTypes.STRING)
+			.addScalar(GradeInfo.EMAIL, StandardBasicTypes.STRING)
+			.setResultTransformer(Transformers.aliasToBean(GradeInfo.class));
+		
+		List<GradeInfo> result = safeList(query);
+		transaction.commit();
+		return result;
+	}
+
+	public void delete(Student student) {
+		Session session = getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			session.remove(student);
+		} catch (Exception e) {
+			transaction.rollback();
+		}
+		transaction.commit();
+	}
+
+}
+// hql => Entity name & attributes
+// native query => return list of entity
+// native query => transformer
+// criteria query
---
+++ lesson18-hibernate-config/src/main/java/view/App.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.util.List;
+
+import dao.HibernateStudentDao;
+import dao.StudentDao;
+import persistence.GradeInfo;
+import persistence.Student;
+
+public class App {
+	
+	private static StudentDao studentDao;
+	
+	static {
+		studentDao = new HibernateStudentDao();
+	}
+	
+	public static void main(String[] args) {
+//		studentDao.save(new Student(2, "Chau", "Ngoc", "ngocchau@gmail.com"));
+
+//		List<GradeInfo> result = studentDao.getGradeInfos();
+//		System.out.println("size: " + result.size());
+		
+//		studentDao.delete(new Student(2, "Chau", "Ngoc", "ngocchau@gmail.com"));
+		List<Student> students = studentDao.getAll();
+		System.out.println("size: " + students.size());
+	}
+}
