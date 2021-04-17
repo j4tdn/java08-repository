bf39e35b3c10c2d005a37b5e31167e713cbba7dd
DauThuy <dauthithuy99@gmail.com>
2021-04-17 17:50:01 +0700

    Remove wrong projects

--- lesson02-variables-datatype/src/main/java/common/Operator.java
+++
@@ -1,5 +0,0 @@
-package common;
-
-public enum Operator {
-	SUM, SUB, MUL, DIV, MOD //tuong trung 5 hang so, SUM: name cua enum
-}
--- lesson02-variables-datatype/src/main/java/thuy/view/Ex04Cilent.java
+++
@@ -1,9 +0,0 @@
-package thuy.view;
-
-public class Ex04Cilent {
-
-	public static void main(String[] args) {
-		Ex03Device.Android();
-	}
-
-}
